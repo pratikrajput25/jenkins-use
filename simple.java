05fa63aedce493e33dd08042c2d790d05e8b72cb
pratikrajput25 <pratik.rajput23@pccoepune.org>
2026-02-09 11:34:26 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,7 +1,16 @@
-import java.util.*;
+// import java.util.*;
 
-class simple{
-    public static void main(String args[]){
-        System.out.println("Hello  java");
+// class simple{
+//     public static void main(String args[]){
+//         System.out.println("Hello  java");
+//     }
+
+// }
+
+package com.pratik.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java 25 and Jenkins!");
     }
 }
